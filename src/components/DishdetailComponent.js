0133c2639167effacb0b5cef4b222463bc1e837e
ToanFX15903 <vtv5hdcq@gmail.com>
2022-05-29 00:59:40 +0700

    Exercise 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -8,8 +8,17 @@ import {
   CardImg,
   Breadcrumb,
   BreadcrumbItem,
+  Row,
+  Label,
+  Col,
+  Button,
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Errors, Control, LocalForm } from "react-redux-form";
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
 
 function RenderDish({ dish }) {
   return (
@@ -25,7 +34,103 @@ function RenderDish({ dish }) {
   );
 }
 
+function ModalSubmit() {
+   
+
+  const HideModal = () => {
+    document.querySelector(".modal-container").style.display = "none";
+    document
+      .querySelector(".LocalForm")
+      .addEventListener("click", function (event) {
+        event.stopPropagation();
+      });
+  };
+
+  return (
+    <div className="modal-container" onClick={HideModal} >
+    <LocalForm
+      className="LocalForm"
+      onSubmit={(values) => this.handleSubmit(values)}
+      >
+      <h2>Submit Comment</h2>
+        <Row className="form-group">
+          <Label htmlFor="yourname" md={12}>
+            Rating
+          </Label>
+          <Col md={{ size: 12 }}>
+            <Control.select
+              model=".contactType"
+              name="contactType"
+              className="form-control"
+            >
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Control.select>
+          </Col>
+        </Row>
+        <Row className="form-group">
+          <Label htmlFor="yourname" md={12}>
+            Your Name
+          </Label>
+          <Col md={{ size: 12 }}>
+            <Control.text
+              model=".yourname"
+              id="yourname"
+              name="yourname"
+              placeholder="Your Name"
+              className="form-control"
+              validators={{
+                required,
+                minLength: minLength(3),
+                maxLength: maxLength(15),
+              }}
+            />
+            <Errors
+              className="text-danger"
+              model=".yourname"
+              show="touched"
+              messages={{
+                required: "Required",
+                minLength: "Must be greater than 2 characters",
+                maxLength: "Must be 15 characters or less",
+              }}
+            />
+          </Col>
+        </Row>
+        <Row className="form-group">
+          <Label htmlFor="feedback" md={12}>
+            Comment
+          </Label>
+          <Col md={{ size: 12 }}>
+            <Control.textarea
+              model=".message"
+              id="message"
+              name="message"
+              rows="12"
+              className="form-control"
+            />
+          </Col>
+        </Row>
+        <Row className="form-group">
+          <Col md={{ size: 10 }}>
+            <Button type="submit" color="primary">
+              Submit
+            </Button>
+          </Col>
+        </Row>
+    </LocalForm>
+      </div>
+  );
+}
+
 function RenderComments({ comments }) {
+  const ShowModalSubmit = () => {
+    return (document.querySelector(".modal-container").style.display = "block");
+  };
+
   console.log(comments);
   if (comments != null)
     return (
@@ -43,6 +148,9 @@ function RenderComments({ comments }) {
             );
           })}
         </ul>
+        <button className="btn-show-modal" onClick={ShowModalSubmit}>
+          Submit Comment
+        </button>
       </div>
     );
   else return <div></div>;
@@ -65,8 +173,9 @@ const DishDetail = (props) => {
           </div>
         </div>
         <div className="row ">
-            <RenderDish dish={props.dish} />
-            <RenderComments comments={props.comments} />
+          <RenderDish dish={props.dish} />
+          <RenderComments comments={props.comments} />
+          <ModalSubmit />
         </div>
       </div>
     );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -18,9 +18,9 @@ const mapStateToProps = (state) => {
   }
 };
 class Main extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     const HomePage = () => {
